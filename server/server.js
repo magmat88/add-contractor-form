edf78d61dab3838a02c97938ba2936c028b43649
magmat88 <magdalena.matusiak88@gmail.com>
2021-12-18 15:20:39 +0100

    Added changes in server and styles.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
-const express = require("express");
+// const https = require('https');
+const express = require('express');
 const port = 60001;
 const app = express();
 
+// https.createServer(app).listen(port);
+
+// https.createServer((req, res) => {
+//     res.status(404);
+//     res.send("Nie znaleziono metody zapisu.");
+// });
 
 app.post('/Contractor/Save', (req, res) => {
     res.status(404);
-    res.send("Nie znaleziono metody zapisu.");
-})
-
-app.listen(port, () => {
+    res.send('Nie znaleziono metody zapisu.');
 });
+
+app.listen(port, () => {});
+
+//package.json: add in scripts: "server": "nodemon \"server/server.js\" --ignore client",
+//package.json: change proxy:     "proxy": "https://localhost:60001"
+
--- src/app/components/form/form.component.tsx
+++ src/app/components/form/form.component.tsx
@@ -10,7 +10,7 @@ enum ContractorType {
     COMPANY = 'Firma',
 }
 
-enum IdentificationNumberType {
+enum IdentificationType {
     PESEL = 'PESEL',
     NIP = 'NIP',
 }
@@ -21,148 +21,151 @@ export function Form({ formName }: FormProps) {
     const [contractorType, setContractorType] = useState<ContractorType>(
         ContractorType.NATURAL_PERSON
     );
-    const [identification, setIdentification] = useState<string>('');
+    const [contractorIdentification, setContractorIdentification] = useState<string>('');
     const [selectedPhoto, setSelectedPhoto] = useState<string>('');
 
     useEffect(() => {}, [contractorType, selectedPhoto]);
 
-    const identificationNumberType =
+    const identificationType =
         contractorType === ContractorType.NATURAL_PERSON
-            ? IdentificationNumberType.PESEL
-            : IdentificationNumberType.NIP;
+            ? IdentificationType.PESEL
+            : IdentificationType.NIP;
 
     //any type - to change
-    function onChangeContractorName(event: any) {
+    function onChangeContractorName(
+        event: React.ChangeEvent<HTMLInputElement>
+    ) {
         setContractorName(event.target.value);
-    };
-    function onSetContractorSurname(event: any) {
+    }
+    function onSetContractorSurname(
+        event: React.ChangeEvent<HTMLInputElement>
+    ) {
         setContractorSurname(event.target.value);
     }
-    function onChangeContractorType(event: any) {
+    function onChangeContractorType(
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) {
         setContractorType(event.target.value as ContractorType);
-    };
-    function onChangeIdentification(event: any) {
-        setIdentification(event.target.value as IdentificationNumberType);
-    };
-    function onChangePhoto(event: any) {
-        const fileType = event.target.files[0].type;
+    }
+    function onChangeIdentification(
+        event: React.ChangeEvent<HTMLInputElement>
+    ) {
+        setContractorIdentification(event.target.value as IdentificationType);
+    }
+    function onChangePhoto(event: React.ChangeEvent<HTMLInputElement>) {
+        const fileType = event.target.files![0].type;
         if (fileType === 'image/jpg' || fileType === 'image/jpeg') {
-            setSelectedPhoto(URL.createObjectURL(event.target.files[0]));
+            setSelectedPhoto(URL.createObjectURL(event.target.files![0]));
         } else {
             alert('Nieprawidłowy format pliku.');
         }
-    };
+    }
 
     return (
         <div className="main-container">
-            <div className="form-container">
-                <div className="form-header">
-                    <h2>{formName}</h2>
-                    <p>
-                        <em>Proszę uzupełnić dane w poniższym formularzu.</em>
-                    </p>
-                </div>
-
-                <form
-                    id="add-contractor-form"
-                    action="./Contractor/Save"
-                    method="POST"
-                >
-                    <section className="form-section">
-                        <label htmlFor="name">
-                            Imię:
-                            <input
-                                type="text"
-                                name="name"
-                                placeholder="Wprowadź imię"
-                                onChange={onChangeContractorName}
-                            />
-                        </label>
-                        <hr />
-                    </section>
-
-                    <section className="form-section">
-                        <label htmlFor="surname">
-                            Nazwisko:
-                            <input
-                                type="text"
-                                name="surname"
-                                placeholder="Wprowadź nazwisko"
-                                onChange={onSetContractorSurname}
-                            />
-                        </label>
-                        <hr />
-                    </section>
-
-                    <section className="form-section">
-                        <label htmlFor="contractorType">
-                            Typ kontrahenta:
-                            <select
-                                value={contractorType}
-                                placeholder="Wybierz typ kontrahenta"
-                                onChange={onChangeContractorType}
-                            >
-                                <option value={ContractorType.NATURAL_PERSON}>
-                                    {ContractorType.NATURAL_PERSON}
-                                </option>
-                                <option value={ContractorType.COMPANY}>
-                                    {ContractorType.COMPANY}
-                                </option>
-                            </select>
-                        </label>
-                        <hr />
-                    </section>
-
-                    <section className="form-section">
-                        <label htmlFor="identificationNumber">
-                            {identificationNumberType}:
-                            <input
-                                type="text"
-                                name={identification}
-                                placeholder={`Wprowadź ${identificationNumberType}`}
-                                pattern="[0-9]+"
-                                minLength={
-                                    identificationNumberType! ===
-                                    IdentificationNumberType.NIP
-                                        ? 10
-                                        : 11
-                                }
-                                maxLength={
-                                    identificationNumberType! ===
-                                    IdentificationNumberType.NIP
-                                        ? 10
-                                        : 11
-                                }
-                                onChange={onChangeIdentification}
-                            />
-                        </label>
-                        <hr />
-                    </section>
-
-                    <section className="form-section">
-                        <label htmlFor="photo">
-                            {' '}
-                            Zdjęcie:
-                            <input
-                                type="file"
-                                name="photo"
-                                accept="image/jpg, image/jpeg"
-                                // accept=".jpg, .jpeg"
-                                onChange={onChangePhoto}
-                            />
-                        </label>
-                        <hr />
-                    </section>
-
-                    <section className="form-section">
-                        <button>Zatwierdź</button>
-                        <hr />
-                    </section>
-                </form>
+            <div className="form-header">
+                <h2>{formName}</h2>
+                <p>
+                    <em>Proszę uzupełnić dane w poniższym formularzu.</em>
+                </p>
             </div>
 
+            <form
+                className="form-container"
+                id="add-contractor-form"
+                action="./Contractor/Save"
+                method="POST"
+            >
+                <section className="form-section">
+                    <label htmlFor="name">
+                        Imię:
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Wprowadź imię"
+                            onChange={onChangeContractorName}
+                        />
+                    </label>
+                </section>
+
+                <section className="form-section">
+                    <label htmlFor="surname">
+                        Nazwisko:
+                        <input
+                            type="text"
+                            name="surname"
+                            placeholder="Wprowadź nazwisko"
+                            onChange={onSetContractorSurname}
+                        />
+                    </label>
+                </section>
+
+                <section className="form-section">
+                    <label htmlFor="contractorType">
+                        Typ kontrahenta:
+                        <select
+                            value={contractorType}
+                            placeholder="Wybierz typ kontrahenta"
+                            onChange={onChangeContractorType}
+                        >
+                            <option value={ContractorType.NATURAL_PERSON}>
+                                {ContractorType.NATURAL_PERSON}
+                            </option>
+                            <option value={ContractorType.COMPANY}>
+                                {ContractorType.COMPANY}
+                            </option>
+                        </select>
+                    </label>
+                </section>
+
+                <section className="form-section">
+                    <label htmlFor="identification">
+                        {identificationType}:
+                        <input
+                            type="text"
+                            name={contractorIdentification}
+                            placeholder={`Wprowadź ${identificationType}`}
+                            pattern="[0-9]+"
+                            minLength={
+                                identificationType! ===
+                                IdentificationType.NIP
+                                    ? 10
+                                    : 11
+                            }
+                            maxLength={
+                                identificationType! ===
+                                IdentificationType.NIP
+                                    ? 10
+                                    : 11
+                            }
+                            onChange={onChangeIdentification}
+                        />
+                    </label>
+                </section>
+
+                <section className="form-section">
+                    <label htmlFor="photo">
+                        {' '}
+                        Zdjęcie:
+                        <input
+                            type="file"
+                            name="photo"
+                            accept="image/jpg, image/jpeg"
+                            onChange={onChangePhoto}
+                        />
+                    </label>
+                </section>
+            </form>
+
             <div className="preview-container">
-                <h2>Podgląd zdjęcia:</h2>
+                <h2>Podgląd wprowadzonych danych:</h2>
+                <div className="text-preview-container">
+                    <p>Imię: {contractorName}</p>
+                    <p>Nazwisko: {contractorSurname}</p>
+                    <p>Typ kontrahenta: {contractorType}</p>
+                    <p>{identificationType}: {contractorIdentification}</p>
+                    <p>Zdjęcie:</p>
+                </div>
                 <div className="img-preview-container">
                     {selectedPhoto && (
                         <img
@@ -173,6 +176,10 @@ export function Form({ formName }: FormProps) {
                     )}
                 </div>
             </div>
+
+            <div className="form-section">
+                <button>Zatwierdź</button>
+            </div>
         </div>
     );
-};
+}
